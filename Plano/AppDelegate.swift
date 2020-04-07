95f20a9e2a1981e368d9de5c5e3c7a7e4f7f392f
Rayhan Faluda <rayhanfaluda@gmail.com>
2020-04-07 14:05:02 +0700

    Added Sign In with Apple view and add a todo view

--- Plano/AppDelegate.swift
+++ Plano/AppDelegate.swift
@@ -8,11 +8,12 @@
 
 import UIKit
 import CoreData
+import AuthenticationServices
 
 @UIApplicationMain
 class AppDelegate: UIResponder, UIApplicationDelegate {
 
-
+    var window: UIWindow?
 
     func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
         // Override point for customization after application launch.
---
+++ Plano/Onboarding/Controller/LandingVC.swift
@@ -0,0 +1,69 @@
+//
+//  LandingVC.swift
+//  Plano
+//
+//  Created by Rayhan Martiza Faluda on 07/04/20.
+//  Copyright © 2020 Mini Challenge 1 - Group 7. All rights reserved.
+//
+
+import UIKit
+import AuthenticationServices
+
+class LandingVC: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+    //MARK: Check if user is signed in or not
+    override func viewDidAppear(_ animated: Bool) {
+        super.viewDidAppear(animated)
+        
+        let appleIDProvider = ASAuthorizationAppleIDProvider()
+        appleIDProvider.getCredentialState(forUserID: KeychainItem.currentUserIdentifier) { (credentialState, error) in
+            switch credentialState {
+            case .authorized:
+                DispatchQueue.main.async {
+                    self.pushTo(viewController: .home)
+                }
+                
+                break // The Apple ID credential is valid.
+            case .revoked, .notFound:
+                // The Apple ID credential is either revoked or was not found, so show the sign-in UI.
+                DispatchQueue.main.async {
+                    self.pushTo(viewController: .welcome)
+                }
+            default:
+                break
+            }
+        }
+    }
+    
+    //MARK: Push to relevant ViewController
+    func pushTo(viewController: ViewControllerType)  {
+        switch viewController {
+        case .home:
+            let vc = self.storyboard?.instantiateViewController(withIdentifier: "TabBar") as! UITabBarController
+            vc.modalPresentationStyle = .fullScreen
+            self.present(vc, animated: false, completion: nil)
+        case .welcome:
+            let vc = self.storyboard?.instantiateViewController(withIdentifier: "onboardVC") as! OnboardVC
+            vc.modalPresentationStyle = .fullScreen
+            self.present(vc, animated: true, completion: nil)
+        }
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
