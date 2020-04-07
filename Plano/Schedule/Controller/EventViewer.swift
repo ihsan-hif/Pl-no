1a43e68ab0c9345767c2ed27b672fd58de55f997
richardreynaldo <richardreynaldo626@gmail.com>
2020-04-07 16:20:33 +0700

    Schedule Calender

---
+++ Plano/Schedule/Controller/EventViewer.swift
@@ -0,0 +1,46 @@
+//
+//  EventViewer.swift
+//  Plano
+//
+//  Created by Laurentius Richard Reynaldo on 07/04/20.
+//  Copyright © 2020 Mini Challenge 1 - Group 7. All rights reserved.
+//
+
+import UIKit
+
+final class EventViewer: UIView {
+    private let textLabel: UILabel = {
+        let label = UILabel()
+        label.font = .systemFont(ofSize: 19)
+        label.textAlignment = .center
+        label.numberOfLines = 0
+        label.text = "Select event to view the description"
+        return label
+    }()
+    
+    var text: String? {
+        didSet {
+            textLabel.text = text
+        }
+    }
+    
+    override init(frame: CGRect) {
+        super.init(frame: frame)
+        if #available(iOS 13.0, *) {
+            backgroundColor = .systemBackground
+        } else {
+            backgroundColor = .white
+        }
+        textLabel.frame = frame
+        addSubview(textLabel)
+    }
+    
+    required init?(coder aDecoder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+    
+    func reloadFrame(frame: CGRect) {
+        textLabel.frame = frame
+    }
+}
+
